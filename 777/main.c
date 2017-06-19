0801b0b907efef988e2fb987de6717bf212bc4e1
20161104573 <a20161104573@admindeiMac-34.local>
2017-06-19 11:40:42 +0800

    成绩管理
    成绩低于60分为不及格

--- 777/main.c
+++ 777/main.c
@@ -3,13 +3,16 @@
 //  777
 //
 //  Created by a20161104573 on 2017/6/19.
-//  Copyright © 2017年 a20161104573. All rights reserved.
-//
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//  Copyright © 2017年 a20161104573. All rights reser
+#include<stdio.h>
+int main()
+{
+    float a;
+        printf("please enter a:");
+        scanf("%f ",&a);
+    if (a>=60)
+         printf("及格\n");
+    else
+         printf("不及格\n");
     return 0;
 }
